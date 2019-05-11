eaea41add1a61ddc66c9678411ddc603955549cb
hyosunghan <ihyosung@qq.com>
2019-05-12 06:06:36 +0800

    fix

---
+++ src/main/java/com/example/demo/entity/SysLog.java
@@ -0,0 +1,68 @@
+package com.example.demo.entity;
+
+public class SysLog {
+
+    private int id;
+    private String visitTime;
+    private String username;
+    private String ip;
+    private String url;
+    private int executionTime;
+    private String method;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVisitTime() {
+        return visitTime;
+    }
+
+    public void setVisitTime(String visitTime) {
+        this.visitTime = visitTime;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getExecutionTime() {
+        return executionTime;
+    }
+
+    public void setExecutionTime(int executionTime) {
+        this.executionTime = executionTime;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
