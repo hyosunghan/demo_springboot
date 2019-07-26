ddc82b75f128f0f9a12a13f76c26f1d3f588b61c
hyosunghan <ihyosung@qq.com>
2019-07-26 13:31:12 +0800

    fix

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -10,27 +10,37 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)  //  启用方法级别的权限认证
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Resource
+    private DataSource dataSource;
+
     @Autowired
     private UsersService usersService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                // 不保护的页面
-                .authorizeRequests().antMatchers("/").permitAll()
-                .antMatchers("/css/**", "/img/**", "/plugins/**").permitAll()   // 静态资源
-                .antMatchers("/swagger-ui.*").permitAll()                    // 接口文档
+                .authorizeRequests()
+                // 不保护的页面及资源
+                .antMatchers("/", "/swagger-ui.*", "/css/**", "/img/**", "/plugins/**").permitAll()
                 // 其余页面需验证
                 .anyRequest().authenticated()
                 // 登录
                 .and().formLogin().loginPage("/").loginProcessingUrl("/login").defaultSuccessUrl("/index").failureUrl("/").permitAll()
                 // 注销
                 .and().logout().logoutUrl("/logout").logoutSuccessUrl("/").permitAll()
+                // 记住我相关配置
+                .and().rememberMe().tokenRepository(persistentTokenRepository()).tokenValiditySeconds(86400)
                 // 关闭csrf保护
                 .and().csrf().disable();
     }
@@ -43,5 +53,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        // 第一次启动的时候自动建表
+        // jdbcTokenRepository.setCreateTableOnStartup(true);
+        return jdbcTokenRepository;
     }
 }
