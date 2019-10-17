c5b712652fde1537237508e9d76a42a3a345f201
hyosunghan <ihyosung@qq.com>
2019-10-17 16:33:08 +0800

    fix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- src/main/java/com/example/demo/aspect/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/aspect/GlobalExceptionHandler.java
@@ -1,15 +1,8 @@
 package com.example.demo.aspect;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.net.BindException;
 
 /**
  * @author hyosunghan
@@ -20,13 +13,12 @@ public class GlobalExceptionHandler {
 
 //    @ResponseBody
 //    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(BindException.class)
-    public ModelAndView bindExceptionHandler(/*Exception e, HttpServletRequest request, HttpServletResponse response*/){
-        return new ModelAndView("content-404");
-    }
-
     @ExceptionHandler(Exception.class)
-    public ModelAndView exceptionHandler(){
-        return new ModelAndView("content-500");
+    public ModelAndView exceptionHandler(/*Exception e,
+                                         HttpStatus status,
+                                         HttpServletRequest request,
+                                         HttpServletResponse response*/){
+
+        return new ModelAndView("content-error");
     }
 }
