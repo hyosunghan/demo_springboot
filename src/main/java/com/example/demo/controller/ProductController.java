64276f48958ae991235f803f3b980cd234cda16d
hyosunghan <ihyosung@qq.com>
2019-10-17 15:10:56 +0800

    fix

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -5,7 +5,6 @@ import com.example.demo.service.ProductService;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,7 +26,6 @@ public class ProductController {
     @RequestMapping(value = "/findAll", method = RequestMethod.GET)
     public ModelAndView findAllProduct(@RequestParam(defaultValue = "1") Integer page,
                                        @RequestParam(defaultValue = "10") Integer size) {
-
         PageInfo<Product> pageInfo = productService.findAll(page, size);
         return new ModelAndView("content-product-list", "pageInfo", pageInfo);
     }
