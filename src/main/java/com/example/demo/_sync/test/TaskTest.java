6b635b1470f8d3cbcba6e0c7653a88916abefd7a
hyosunghan <ihyosung@qq.com>
2021-08-05 19:15:40 +0800

    fix

--- src/main/java/com/example/demo/_sync/test/TaskTest.java
+++ src/main/java/com/example/demo/_sync/test/TaskTest.java
@@ -16,34 +16,6 @@ public class TaskTest {
 
     private static final int JOB_LENGTH = 100;
 
-    private static class QueryTest implements Runnable {
-        private PendingJobPool pendingJobPool;
-
-        public QueryTest(PendingJobPool pendingJobPool) {
-            this.pendingJobPool = pendingJobPool;
-        }
-
-        @Override
-        public void run() {
-            for (int i = 0; i < 15; i++) {
-                int[] taskPercent = pendingJobPool.getTaskPercent(JOB_NAME);
-                List<TaskResult<String>> taskDetail = pendingJobPool.getTaskDetail(JOB_NAME);
-                if (taskPercent == null || taskDetail == null) {
-                    System.out.println("第" + (i + 1) + "次查询：任务[" + JOB_NAME + "]已过期");
-                } else if (!taskDetail.isEmpty()) {
-                    System.out.println("第" + (i + 1) + "次查询：任务[" + JOB_NAME + "]进度[总数" + taskPercent[0] + "完成" + taskPercent[1] + "成功" + taskPercent[2] + "]详情: " + JSON.toJSONString(taskDetail));
-                } else {
-                    System.out.println("第" + (i + 1) + "次查询：任务[" + JOB_NAME + "]暂无数据");
-                }
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
         // 业务逻辑
         TaskProcessorImpl taskProcessor = new TaskProcessorImpl();
@@ -56,7 +28,22 @@ public class TaskTest {
         }
 
         // 查询结果
-        new Thread(new QueryTest(instance)).start();
+        for (int i = 0; i < 15; i++) {
+            int[] taskPercent = instance.getTaskPercent(JOB_NAME);
+            List<TaskResult<String>> taskDetail = instance.getTaskDetail(JOB_NAME);
+            if (taskPercent == null || taskDetail == null) {
+                System.out.println("第" + (i + 1) + "次查询：任务[" + JOB_NAME + "]已过期");
+            } else if (!taskDetail.isEmpty()) {
+                System.out.println("第" + (i + 1) + "次查询：任务[" + JOB_NAME + "]进度[总数" + taskPercent[0] + "完成" + taskPercent[1] + "成功" + taskPercent[2] + "]详情: " + JSON.toJSONString(taskDetail));
+            } else {
+                System.out.println("第" + (i + 1) + "次查询：任务[" + JOB_NAME + "]暂无数据");
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
