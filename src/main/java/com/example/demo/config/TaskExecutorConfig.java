97e63e5f5e04297ccc9c3f6b241fb1b8a21b3ee0
hyosunghan <ihyosung@qq.com>
2024-09-30 00:03:54 +0800

    fix

--- src/main/java/com/example/demo/config/TaskExecutorConfig.java
+++ src/main/java/com/example/demo/config/TaskExecutorConfig.java
@@ -9,35 +9,10 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
-@EnableAsync
+//@EnableAsync
 @Configuration
 public class TaskExecutorConfig {
 
-    @Value("${task.executor.pool.size:10}")
-    private Integer corePoolSize;
 
-    @Value("${task.executor.queue.size:10000}")
-    private Integer queueSize;
-
-    @Bean
-    @Primary
-    public ThreadPoolTaskExecutor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        // 设置核心线程数
-        executor.setCorePoolSize(corePoolSize);
-        // 设置最大线程数
-        executor.setMaxPoolSize(corePoolSize);
-        // 设置队列容量
-        executor.setQueueCapacity(queueSize);
-        // 设置线程活跃时间（秒）
-        executor.setKeepAliveSeconds(60);
-        // 设置默认线程名称
-        executor.setThreadNamePrefix("hre-task-pool");
-        // 设置拒绝策略
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        // 等待所有任务结束后再关闭线程池
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        return executor;
-    }
 
 }
--- src/main/java/com/example/demo/config/ThreadPoolConfig.java
+++ src/main/java/com/example/demo/config/ThreadPoolConfig.java
@@ -1,11 +1,13 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
@@ -16,8 +18,35 @@ import java.util.concurrent.ThreadPoolExecutor;
 @Configuration
 public class ThreadPoolConfig {
 
-    @Bean("taskExecutor")
-    public Executor taskExecutor() {
+    @Value("${task.executor.pool.size:10}")
+    private Integer corePoolSize;
+
+    @Value("${task.executor.queue.size:10000}")
+    private Integer queueSize;
+
+    @Bean
+    @Primary
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        // 设置核心线程数
+        executor.setCorePoolSize(corePoolSize);
+        // 设置最大线程数
+        executor.setMaxPoolSize(corePoolSize);
+        // 设置队列容量
+        executor.setQueueCapacity(queueSize);
+        // 设置线程活跃时间（秒）
+        executor.setKeepAliveSeconds(60);
+        // 设置默认线程名称
+        executor.setThreadNamePrefix("threadPoolTaskExecutor");
+        // 设置拒绝策略
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 等待所有任务结束后再关闭线程池
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        return executor;
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
         // 创建一个线程池对象
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         // 定义一个线程池大小
